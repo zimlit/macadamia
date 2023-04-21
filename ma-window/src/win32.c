857477d87b126edfce801b4421f4169e8c0a7efc
zimlit <zimlit2010@gmail.com>
2023-04-21 00:23:20 -0400

    attempt to load context on windows

--- ma-window/src/win32.c
+++ ma-window/src/win32.c
@@ -1,6 +1,8 @@
 #include <ma-window/ma-window.h>
 #include <windows.h>
 #include <stdlib.h>
+#include <glad/wgl.h>
+#include <glad/gl.h>
 
 #ifndef UNICODE
 #define UNICODE
@@ -11,6 +13,7 @@ typedef struct {
     HWND hwnd;
     wchar_t *title_w;
     bool shoud_close;
+    HINSTANCE hInstance;
 } Win32Window;
 
 LRESULT CALLBACK maWindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
@@ -41,6 +44,7 @@ MaWindow *maWindowNew(int width, int height, const char *title) {
     window->shoud_close = false;
 
     HINSTANCE hInstance = GetModuleHandle(NULL);
+    window->hInstance = hInstance;
 
     const wchar_t CLASS_NAME[] = L"ma-window";
     WNDCLASSW wc = {0};
@@ -109,7 +113,112 @@ bool maWindowPollEvents(MaWindow *window) {
 void maWindowMakeGlContextCurrent(MaWindow *window) {
 }
 
-bool maWindowMakeGlContext(MaWindow *window, int major, int minor) {
+bool maWindowMakeGlContext(MaWindow *pwindow, int major, int minor) {
+    Win32Window *window = (Win32Window *)pwindow;
+    HDC DC = GetDC(window->hwnd);
+
+    HWND fakeWND = CreateWindow(
+        "Core", "Fake Window",      // window class, title
+        WS_CLIPSIBLINGS | WS_CLIPCHILDREN, // style
+        0, 0,                       // position x, y
+        1, 1,                       // width, height
+        NULL, NULL,                 // parent window, menu
+        window->hInstance, NULL);           // instance, param
+    if (!fakeWND) {
+        free(window);
+        return false;
+    }
+
+    HDC fakeDC = GetDC(fakeWND);
+    PIXELFORMATDESCRIPTOR fakePFD;
+    ZeroMemory(&fakePFD, sizeof(fakePFD));
+    fakePFD.nSize = sizeof(fakePFD);
+    fakePFD.nVersion = 1;
+    fakePFD.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+    fakePFD.iPixelType = PFD_TYPE_RGBA;
+    fakePFD.cColorBits = 32;
+    fakePFD.cAlphaBits = 8;
+    fakePFD.cDepthBits = 24;
+    
+    int fakePFDID = ChoosePixelFormat(fakeDC, &fakePFD);
+    if (fakePFDID == 0) {
+        DestroyWindow(fakeWND);
+        free(window);
+        return false;
+    }
+    if (!SetPixelFormat(fakeDC, fakePFDID, &fakePFD)) {
+        DestroyWindow(fakeWND);
+        free(window);
+        return false;
+    }
+    HGLRC fakeRC = wglCreateContext(fakeDC);
+    if (!fakeRC) {
+        DestroyWindow(fakeWND);
+        free(window);
+        return false;
+    }
+    if (!wglMakeCurrent(fakeDC, fakeRC)) {
+        wglDeleteContext(fakeRC);
+        DestroyWindow(fakeWND);
+        free(window);
+        return false;
+    }
+
+    gladLoadWGL(fakeDC, (GLADloadfunc)wglGetProcAddress);
+
+    const int pixelAttribs[] = {
+        WGL_DRAW_TO_WINDOW_ARB, GL_TRUE,
+        WGL_SUPPORT_OPENGL_ARB, GL_TRUE,
+        WGL_DOUBLE_BUFFER_ARB, GL_TRUE,
+        WGL_PIXEL_TYPE_ARB, WGL_TYPE_RGBA_ARB,
+        WGL_ACCELERATION_ARB, WGL_FULL_ACCELERATION_ARB,
+        WGL_COLOR_BITS_ARB, 32,
+        WGL_ALPHA_BITS_ARB, 8,
+        WGL_DEPTH_BITS_ARB, 24,
+        WGL_STENCIL_BITS_ARB, 8,
+        WGL_SAMPLE_BUFFERS_ARB, GL_TRUE,
+        WGL_SAMPLES_ARB, 4,
+        0
+    };
+    
+    int pixelFormatID; UINT numFormats;
+    bool status = wglChoosePixelFormatARB(DC, pixelAttribs, NULL, 1, &pixelFormatID, &numFormats);
+    
+    if (!status || numFormats == 0) {
+        DestroyWindow(window->hwnd);
+        free(window);
+        return false;
+    }
+
+    PIXELFORMATDESCRIPTOR PFD;
+    DescribePixelFormat(DC, pixelFormatID, sizeof(PFD), &PFD);
+    SetPixelFormat(DC, pixelFormatID, &PFD);
+
+    int  contextAttribs[] = {
+        WGL_CONTEXT_MAJOR_VERSION_ARB, major,
+        WGL_CONTEXT_MINOR_VERSION_ARB, minor,
+        WGL_CONTEXT_PROFILE_MASK_ARB, WGL_CONTEXT_CORE_PROFILE_BIT_ARB,
+        0
+    };
+    
+    HGLRC RC = wglCreateContextAttribsARB(DC, 0, contextAttribs);
+    if (RC == NULL) {
+        DestroyWindow(window->hwnd);
+        maWindowFree(pwindow);
+        return false;
+    }
+
+    wglMakeCurrent(NULL, NULL);
+    wglDeleteContext(fakeRC);
+    ReleaseDC(fakeWND, fakeDC);
+    DestroyWindow(fakeWND);
+
+    if (!wglMakeCurrent(DC, RC)) {
+        wglDeleteContext(RC);
+        DestroyWindow(window->hwnd);
+        maWindowFree(pwindow);
+        return false;
+    }
     return true;
 }
 
